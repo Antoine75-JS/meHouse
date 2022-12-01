108ef6733bebbaf44e8adb588b7b6f509d507b35
groot <garci.ntoine@gmail.com>
2022-12-01 13:49:24 +0100

    feature(client) - login form

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 // Components
 import Homepage from '../Homepage';
 import TaskEditPage from '../TaskEditPage';
 import TaskPage from '../Tasks/TaskPage';
-
+import LoginPage from '../Login';
 // Redux components
 // import LoadingComponent from '../../containers/loadingContainer';
 import Loading from '../Utils/Loading';
@@ -18,10 +18,12 @@ const App: React.FC = () => {
       {isLoading && <Loading />}
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='/:id'>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/user/:id'>
           <Route index element={<TaskPage />} />
           <Route index path='edit' element={<TaskEditPage />} />
         </Route>
+        <Route path='*' element={<Homepage />} />
       </Routes>
     </div>
   );
---
+++ client/src/components/Login/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+// Ts
+interface FormInputs {
+  email: string;
+  password: string;
+}
+
+const LoginPage: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormInputs>();
+
+  const handleLogin: SubmitHandler<FormInputs> = (data: FormInputs) => {
+    console.log('data', data);
+  };
+
+  return (
+    <div className='flex-col flex justify-center items-center'>
+      LoginPage
+      <form
+        onSubmit={handleSubmit(handleLogin)}
+        className='flex flex-col justify-center items-center gap-4'
+      >
+        <div className='w-600 '>
+          <label
+            htmlFor='email'
+            className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+          >
+            Email
+            <input
+              type='text'
+              id='email'
+              {...register('email')}
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              placeholder='your@email.com'
+              required
+            />
+          </label>
+          <label
+            htmlFor='password'
+            className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+          >
+            Password :
+            <input
+              type='password'
+              id='password'
+              {...register('password')}
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              required
+            />
+          </label>
+        </div>
+        {/* {errors && <span>This field is required</span>} */}
+        <button
+          type='submit'
+          className='text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
