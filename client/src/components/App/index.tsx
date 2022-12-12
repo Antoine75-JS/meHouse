00908488f91b7697afdf691b6355c7678fbd0c5b
groot <garci.ntoine@gmail.com>
2022-12-12 18:19:18 +0100

    feature(client) - lazy + suspense homepage router

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -1,22 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
 // Components
-import Homepage from '../Homepage';
-import TaskEditPage from '../Tasks/EditTaskForm';
-import TaskPage from '../Tasks/TaskPage';
-import LoginPage from '../LoginPage';
-// Redux components
-// import LoadingComponent from '../../containers/loadingContainer';
 import Loading from '../Utils/Loading';
 import { checkUserLogged } from '../../actions/auth';
 import Snackbar from '../Utils/Snackbar';
 import Navbar from '../Navbar';
-import OrganisationHomepage from '../Organisations/OrgaHomepage';
-import NewTaskForm from '../Tasks/NewTaskForm';
-import SignupPage from '../SignupPage';
-import CreateOrganisationForm from '../Organisations/CreateOrganisationForm';
+
+// LAZY
+const OrganisationHomepage = lazy(() => import('../Organisations/OrgaHomepage'));
+const LoginPage = lazy(() => import('../LoginPage'));
+const TaskEditPage = lazy(() => import('../Tasks/EditTaskForm'));
+const TaskPage = lazy(() => import('../Tasks/TaskPage'));
+const NewTaskForm = lazy(() => import('../Tasks/NewTaskForm'));
+const Homepage = lazy(() => import('../Homepage'));
+const SignupPage = lazy(() => import('../SignupPage'));
+const CreateOrganisationForm = lazy(() => import('../Organisations/CreateOrganisationForm'));
 
 const App: React.FC = () => {
   const isLogged = useSelector((state: IState) => state.user.isLogged);
@@ -39,21 +39,83 @@ const App: React.FC = () => {
         {isLogged && (
           <>
             <Route path='/task'>
-              <Route index element={<TaskPage />} />
-              <Route path=':id' element={<TaskPage />} />
-              <Route path=':id/edit' element={<TaskEditPage />} />
-              <Route path='new/:id' element={<NewTaskForm />} />
+              <Route
+                path=':id'
+                element={
+                  <Suspense fallback={<Loading />}>
+                    <TaskPage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path=':id/edit'
+                element={
+                  <Suspense fallback={<Loading />}>
+                    <TaskEditPage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path='new/:id'
+                element={
+                  <Suspense fallback={<Loading />}>
+                    <NewTaskForm />
+                  </Suspense>
+                }
+              />
             </Route>
             <Route path='/orga'>
-              <Route path=':id' element={<OrganisationHomepage />} />
-              <Route path='new' element={<CreateOrganisationForm />} />
+              <Route
+                path=':id'
+                element={
+                  <Suspense fallback={<Loading />}>
+                    <OrganisationHomepage />
+                  </Suspense>
+                }
+              />
+              <Route
+                path='new'
+                element={
+                  <Suspense fallback={<Loading />}>
+                    <CreateOrganisationForm />
+                  </Suspense>
+                }
+              />
             </Route>
           </>
         )}
-        <Route path='/' element={<Homepage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path='*' element={<Homepage />} />
+        <Route
+          path='/'
+          element={
+            <Suspense fallback={<Loading />}>
+              <Homepage />
+            </Suspense>
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <Suspense fallback={<Loading />}>
+              <LoginPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path='/signup'
+          element={
+            <Suspense fallback={<Loading />}>
+              <SignupPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path='*'
+          element={
+            <Suspense fallback={<Loading />}>
+              <Homepage />
+            </Suspense>
+          }
+        />
       </Routes>
     </div>
   );
