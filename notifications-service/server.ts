00c4abf77e39cb952c3876003e49ae56bdd890ab
groot <garci.ntoine@gmail.com>
2022-12-17 12:55:40 +0100

    feature(notifications) - plug notifications api to docker db

--- notifications-service/server.ts
+++ notifications-service/server.ts
@@ -7,8 +7,33 @@ require('dotenv').config();
 const routers = require('./src/routers');
 
 const app = express();
-
 app.use(express.json());
+
+// Mongo database
+const dbUrl = `${process.env.MONGO_IP}://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.DB_DOCKER_SERVICE_NAME}:${process.env.MONGO_PORT}/${process.env.DB_NAME}?authSource=${process.env.MONGO_ROLE}`;
+console.log('dbUrl', dbUrl);
+
+// Tries to connect, waits 3 secs if error then retries
+const loopUntilConnected = () => {
+  let cpt = 1;
+  mongoose
+    .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(
+      () =>
+        console.log(
+          `Connected to mongo database as ${process.env.MONGO_ROLE} on try number ${cpt}`
+        )
+      // eslint-disable-next-line function-paren-newline
+    )
+    .catch((err: object) => {
+      console.log(err);
+      cpt += 1;
+      setTimeout(loopUntilConnected, 3000);
+    });
+};
+
+// Start connection loop
+loopUntilConnected();
 
 const options = {
   origin: (origin: any, callback: any) => {
