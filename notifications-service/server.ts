b74d8a513619c209a14962bddf264c5a5983c6d5
groot <garci.ntoine@gmail.com>
2022-12-22 10:31:19 +0100

    feature(rabbitMQ) - setup rabbitMQ container + send / consume message from api / notifications

--- notifications-service/server.ts
+++ notifications-service/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 const mongoose = require('mongoose');
+const amqp = require('amqplib/callback_api');
 
 const cors = require('cors');
 require('dotenv').config();
@@ -11,7 +12,6 @@ app.use(express.json());
 
 // Mongo database
 const dbUrl = `${process.env.MONGO_IP}://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.DB_DOCKER_SERVICE_NAME}:${process.env.MONGO_PORT}/${process.env.DB_NAME}?authSource=${process.env.MONGO_ROLE}`;
-console.log('dbUrl', dbUrl);
 
 // Tries to connect, waits 3 secs if error then retries
 const loopUntilConnected = () => {
@@ -52,6 +52,51 @@ app.use(cors(options));
 app.enable('trust proxy');
 
 app.use('/notifications', routers);
+
+const connectRabbit = async () => {
+  await amqp.connect(
+    process.env.RABBIT_HOSTNAME,
+    function (
+      error0: any,
+      connection: {
+        createChannel: (arg0: (error1: any, channel: any) => void) => void;
+      }
+    ) {
+      if (error0) {
+        throw error0;
+      }
+      connection.createChannel(function (error1, channel) {
+        if (error1) {
+          throw error1;
+        }
+
+        channel.assertQueue(process.env.RABBIT_QUEUE_NAME, {
+          durable: false
+        });
+
+        console.log(
+          ' [*] Waiting for messages in %s. To exit press CTRL+C',
+          process.env.RABBIT_QUEUE_NAME
+        );
+
+        channel.consume(
+          process.env.RABBIT_QUEUE_NAME,
+          function (msg: { content: { toString: () => any } }) {
+            console.log(' [x] Received %s', msg.content.toString());
+          },
+          {
+            noAck: true
+          }
+        );
+      });
+    }
+  );
+};
+
+setTimeout(() => {
+  console.log('connecting rabbit notifications consumer');
+  connectRabbit();
+}, 6000);
 
 const fallbackPort = 3405;
 app.listen(process.env.NOTIFICATIONS_PORT || fallbackPort, () => {
